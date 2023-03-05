575162a00428c9d73cab7fc3f2663d0ab3ed4074
JFM <jfmmichel@hotmail.com>
2023-03-05 20:59:31 +0100

    Create ItemCategory to ease identification of different cases

---
+++ Java/src/main/java/com/gildedrose/ItemCategory.java
@@ -0,0 +1,45 @@
+package com.gildedrose;
+
+/**
+ *
+ * @author pcjfm
+ */
+public enum ItemCategory {
+    USUAL("usual"),
+    SULFURAS("Sulfuras", 0),
+    AGED_BRIE("Aged Brie"),
+    BACKSTAGE_PASSES("Backstage passes");
+    
+    private final int USUAL_QUALITY_STEP = 1;
+
+    private final String name;
+    private final int dailyQualityStep;
+
+    private ItemCategory(String name, int dailyQualityStep) {
+        this.name = name;
+        this.dailyQualityStep = dailyQualityStep;
+    }
+
+    private ItemCategory(String name) {
+        this.name = name;
+        this.dailyQualityStep = USUAL_QUALITY_STEP;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDailyQualityStep() {
+        return dailyQualityStep;
+    }
+
+    public static ItemCategory getCategory(String name) {
+        for (ItemCategory category : ItemCategory.values()) {
+            if (category.getName().equalsIgnoreCase(name.trim())) {
+                return category;
+            }
+        }
+        return USUAL;
+    }
+
+}
